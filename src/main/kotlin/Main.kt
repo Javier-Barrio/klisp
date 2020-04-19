e3008809d4390dc1768cfe151fdb988ef46d0a6a
Javier Barrio <=>
2020-04-19 14:10:51 +0200

    Actually fix gradle's build

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 // Klisp. A simple lisp interpreter based on lispy.py (see http://norvig.com/lispy.html)
 
+package klisp.main
+
 import klisp.env.Environment
 import klisp.eval
 import klisp.parser.parse
 import klisp.parser.read
 import java.lang.RuntimeException
-
 
 fun main() {
     println("Hello, this is Klisp 1.0 (`exit` to quit)")
